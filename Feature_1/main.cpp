b3345f9a7bd92ff13f1103e613c01cb8983ef4a1
BubblyFire <ncd.kuipers@student.avans.nl>
2020-11-26 14:42:42 +0100

    Create main.cpp
    Lees the command line en print de ingevoerde string.

---
+++ Feature_1/main.cpp
@@ -0,0 +1,13 @@
+// Feature_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include  <string>
+using namespace std;
+
+int main()
+{
+    string Read;
+    getline(cin, Read);
+    std::cout << Read <<std::endl;
+}
